d6084351d185436ace1138c899c1ec24d69b4d0c
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-17 00:10:32 -0800

    quick fix

--- Composing Methods 2/split_temp_variable.py
+++ Composing Methods 2/split_temp_variable.py
@@ -4,19 +4,17 @@
 class Sandwhich:
     """Represents ingredients of a sandwhich"""
 
-    def __init__(self, patty, pickle, tomatoes,
-                 lettuce, buns):
+    def __init__(self, patty, pickle, tomatoes, buns):
         self.patty = patty
         self.pickle = pickle
         self.tomatoes = tomatoes
-        self.lettuce = lettuce
         self.buns = buns
 
-    def ny_sandwhich(self):
+    def ny_sandwhich(self, lettuce):
         """Returns the weight of New York sandwhich"""
 
         ny_sandwhich = (2 * self.patty + 4 * self.pickle + 3 * self.tomatoes + 2
-                        * self.lettuce + 2 * self.buns)
+                        * lettuce + 2 * self.buns)
         return print("NY Burger Weight", ny_sandwhich)
 
     def kimchi_sandwhich(self, mayo, golden_onion, kimchi):
@@ -27,5 +25,5 @@ class Sandwhich:
         return print("Seoul Kimchi Burger Weight", kimchi_sandwhich)
 
 
-sandwhich_one = Sandwhich(70, 20, 25, 15, 95).ny_sandwhich()
-sandwhich_two = Sandwhich(70, 20, 25, 15, 95).kimchi_sandwhich(30, 5, 20)
+sandwhich_one = Sandwhich(70, 20, 15, 95).ny_sandwhich(25)
+sandwhich_two = Sandwhich(70, 20, 15, 95).kimchi_sandwhich(5, 20, 30)
