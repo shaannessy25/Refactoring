91d40a934c9ba5735cc21693f81ae2062732e48e
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-24 04:07:44 -0800

    finished challenged 13

--- Simplifying Conditional Expressions/consolidate_duplicate_conditional_fragments.py
+++ Simplifying Conditional Expressions/consolidate_duplicate_conditional_fragments.py
@@ -1,25 +1,36 @@
+"""This file returns ingredients of a drink"""
 
-# by Kami Bigdely
-# Consolidate duplicate conditional fragments
-def add(mix, something):
-    mix.append(something)
-    return mix
 
-def mixer_ice_with_cream():
-    print('mixed ice with cream.')
-    return ['ice', 'cream']
+class Drink:
+    """Represents a standard drink"""
 
-def make_drink(drink, addons):
-    if 'coffee' in drink:
-        mix = []
-        mix = add(mix, 'coffee')
-        mix = add(mix, addons)
-    if 'strawberry milkshake' in drink:
-        mix = []
-        mix = mixer_ice_with_cream()
-        mix = add(mix, 'strawberry')
-        mix = add(mix, addons)
-    return mix
+    def __init__(self, drink, addons):
+        self.drink = drink
+        self.addons = addons
+        self.mix = []
 
-final_drink = make_drink('strawberry milkshake', ['milk','sugar'])
-print(final_drink)
+    def add(self, ingredient):
+        """Adds ingredients to mix list"""
+        return self.mix.append(ingredient)
+
+    def mixer_ice_with_cream(self):
+        """Adds ice cream to list returns statement"""
+        self.add('ice')
+        self.add('cream')
+        return print('mixed with ice cream')
+
+    def make_drink(self):
+        """Method returns list of ingredients"""
+        if 'coffee' in self.drink:
+            self.add('coffee')
+            self.add(self.addons)
+        if 'strawberry milkshake' in self.drink:
+            self.mixer_ice_with_cream()
+            self.add('strawberry')
+            self.add(self.addons)
+        return self.mix
+
+
+milkshake = Drink('strawberry milkshake', ['milk', 'sugar']).make_drink()
+
+print(milkshake)
