31fe3405a715f9ce066d7b7a57fc42e37c594919
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-24 06:36:13 -0800

    finished challenge 20

--- Simplifying Conditional Expressions/replace_nested_conditional_with_guard_clauses.py
+++ Simplifying Conditional Expressions/replace_nested_conditional_with_guard_clauses.py
@@ -1,22 +1,19 @@
-# by Kami Bigdely
-# Replace nested conditional with guard clauses
+"""This file contains a function that will extract a position"""
+
 
 def extract_position(line):
-    if not line:
-        pos = None
-    else:
-        if 'debug' in line or 'error' in line:
-            pos = None
-        else:
-            if 'x:' in line:
-                start_index = line.find('x:') + 2
-                pos = line[start_index:] # from start_index to the end.
-            else: 
-                pos = None
+    """Increment position based on line"""
+    pos = 0
+    if 'x:' in line:
+        start_index = line.find('x:') + 2
+        pos = line[start_index:]  # from start_index to the end.
     return pos
 
+
 if __name__ == "__main__":
-    result1 = extract_position('|error| numerical calculations could not converge.')
+    result1 = extract_position(
+        '|error| numerical calculations could not converge.')
     print(result1)
-    result2 = extract_position('|update| the positron location in the particle accelerator is x:21.432')
+    result2 = extract_position(
+        '|update| the positron location in the particle accelerator is x:21.432')
     print(result2)
--- Simplifying Conditional Expressions/replace_nested_conditional_with_guard_clauses2.py
+++ Simplifying Conditional Expressions/replace_nested_conditional_with_guard_clauses2.py
@@ -1,14 +1,17 @@
 
-# Adapted from a Java code in the "Refactoring" book by Martin Fowler.
-# Replace nested conditional with guard clases.
-ADJ_FACTOR = 0.7
+"""This file finds adjusted capital"""
+
+
 def get_adjusted_capital(capital, rate, duration, income):
+    """This function calculates capital"""
+
     result = 0
-    if (capital > 0):
-        if (rate > 0 and duration > 0):
-            result = (income / duration) * ADJ_FACTOR
-    
+    ADJUSTMENT_FACTOR = 0.7
+    if (capital > 0 or (rate > 0 and duration > 0)):
+        result = (income / duration) * ADJUSTMENT_FACTOR
     return result
 
-adjusted_capital = get_adjusted_capital(50000, 4,10, 10000)
+
+adjusted_capital = get_adjusted_capital(50000, 4, 10, 10000)
+print(adjusted_capital)
 print(adjusted_capital)
