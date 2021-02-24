d5a86e3c57376b4b6521b3aa840b7d8ef83936a1
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-24 03:38:18 -0800

    finished challenge 12

--- Simplifying Conditional Expressions/consolidate_conditional.py
+++ Simplifying Conditional Expressions/consolidate_conditional.py
@@ -1,32 +1,16 @@
-# by Kami Bigdely
-# Consolidate conditional expressions
-def dice(ingredients):
-    print("diced all ingredients.")
-def mix_all(diced_ingredients):
-    print("mixed all.")
-def add_salt():
-    print('added salt.')
-def pour(liquid):
-    print('poured', liquid + '.',)
+
+"""This file uses a method to return a statement"""
+
 
 def make_shirazi_salad(ingredients):
-    if 'cucumber' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'tomato' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'onion' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'lemon juice' not in ingredients:
-        print('lacks ingredients.')
-        return
-    dice(ingredients)
-    mix_all(ingredients)
-    add_salt()
-    pour('lemon juice')
+    """This method will check ingediants and print a statement"""
+    food = ['cucumber', 'tomato', 'onion', 'lemon juice']
+    for item in food:
+        if item not in ingredients:
+            print("lacks ingredients")
+    print("Diced, mixed, and added salt to all ingredients")
     print('Your yummy shirazi salad is ready!')
+
 
 if __name__ == "__main__":
     make_shirazi_salad(['cucumber', 'tomato', 'lemon juice', 'onion'])
