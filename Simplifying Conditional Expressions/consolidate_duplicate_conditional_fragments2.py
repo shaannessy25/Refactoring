99bfc411d5127fa6afbdc5581708a7e465e250e3
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-24 04:58:12 -0800

    finished challenge 14

--- Simplifying Conditional Expressions/consolidate_duplicate_conditional_fragments2.py
+++ Simplifying Conditional Expressions/consolidate_duplicate_conditional_fragments2.py
@@ -1,34 +1,35 @@
+"""This file takes returns a statement based on initial values"""
 
-# By Kami Bigdely-Shamloo
-# Consolidate duplicate conditional fragments
-# This program changes car's gear according to the car speed. Then it 
-# displays the updated gear on the car's front panel.
 
-def change_gear(str_gear):
-    print("Gear changed to", str_gear)
+class Gear:
+    """Class represents car gears"""
 
-def display_gear(str_gear): 
-    print("displayed gear:", str_gear)
+    def __init__(self, speed, gear=1):
+        self.speed = speed
+        self.gear = gear
 
-def process_speed(speed):
-    if 0 <= speed < 30:
-        change_gear('1')
-        gear = '1'
-        display_gear(gear)
-    elif 30 <= speed < 50:
-        change_gear('2')
-        gear = '2'
-        display_gear(gear)
-    elif 50 <= speed <= 90:
-        change_gear('3')
-        gear = '3'
-        display_gear(gear)
-    elif 90 <= speed:
-        change_gear('4')
-        gear = '4'
-        display_gear(gear)
-    elif speed <= 0:
-        display_gear('R')
+    def change_gear(self):
+        """Returns a string"""
+        return f"Gear changed to: {self.gear}"
 
-if __name__ == "__main__":
-    process_speed(40)
+    def display_gear(self):
+        """Returns a string"""
+        return f"Displayed gear: {self.gear}"
+
+    def process_speed(self):
+        """This method checks speed, then updates and displays gears"""
+        if self.speed >= 0 or self.speed < 30:
+            self.gear = 1
+        elif self.speed >= 30 or self.speed < 50:
+            self.gear = 2
+        elif self.speed >= 50 or self.speed <= 90:
+            self.gear = 3
+        elif self.speed >= 90:
+            self.gear = 4
+        elif self.speed <= 0:
+            self.gear = 'R'
+        return f"{self.change_gear()} \n{self.display_gear()}"
+
+
+car_speed = Gear(-5).process_speed()
+print(car_speed)
