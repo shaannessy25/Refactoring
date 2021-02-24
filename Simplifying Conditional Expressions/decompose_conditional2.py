2aefb0b21d1c54de64c9439bc4a879362ff2195b
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-24 05:46:58 -0800

    finished challenge 16

--- Simplifying Conditional Expressions/decompose_conditional2.py
+++ Simplifying Conditional Expressions/decompose_conditional2.py
@@ -1,27 +1,26 @@
-# By Kami Bigdely
-# Decompose conditional
-# Reference: https://www.healthline.com/health/high-cholesterol/levels-by-age
+"""This file determines health based on chlostrol"""
 
-# Blood test analysis program
-total_cholostrol = 70
-ldl = 30
-triglyceride = 120
 
-if total_cholostrol < 200 and ldl < 100 and triglyceride < 150:
-    # good level
-    print('*** Good level of cholestrol ***')
-elif 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200:
-    # High cholestrol level
-    print('*** High cholestrol level ***')
-    print('start taking pills such as statins')
-    print('start TLC diet')
-elif 200 <total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200:
-    #TLC_diet
-    print('*** Borderline to moderately elevated ***')
-    print("Start TLC diet")
-    print("Under this meal plan, only 7 percent of your daily calories \nshould come from saturated fat.")
-    print('Some foods help your digestive tract absorb less cholesterol. For example,\nyour doctor may encourage you to eat more:')
-    print('oats, barley, and other whole grains.')
-    print('fruits such as apples, pears, bananas, and oranges.')
-else:
-    print('Error: unhandled case.')
+def get_cholostrol(total_cholostrol, ldl, triglyceride):
+    """This function checks inputs and returns a string"""
+
+    if total_cholostrol < 200 and ldl < 100 and triglyceride < 150:
+        return "*** Good level of cholestrol ***"
+
+    elif 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200:
+        # High cholestrol level
+        return "*** High cholestrol level *** \nstart taking pills such as statins \nstart TlC diet"
+
+    else:
+        return "*** Borderline to moderately elevated ***\nStart TLC diet"\
+            "\nUnder this meal plan, only 7 percent of your daily calories"\
+            "\nshould come from saturated fat. Some foods help your"\
+            "digestive tract absorb less cholesterol. For example,"\
+            "\nyour doctor may encourage you to eat more: oats, barley,"\
+            "and other whole grains. \nfruits such as apples, pears,"\
+            "bananas, and oranges."
+
+    return 'Error: unhandled case.'
+
+
+print(get_cholostrol(220, 150, 180))
